c174be811461bd592279c0cf7cf49f72511de213
Eric <vespers-garages.0n@icloud.com>
2025-05-19 17:18:13 -0400

    added missing RETURN (not implicit) and added try catch

--- scipts/export-data.js
+++ scipts/export-data.js
@@ -2,9 +2,22 @@ const fs = require('fs').promises;
 const opt = require('../config.js');
 
 async function write(data) {
-  // add
-  // if the export file location is not set to use the import one
-  await fs.writeFile(opt.exportFileLocation, data, opt.format);
+  try {
+    return await fs.writeFile(
+      opt.exportFileLocation,
+      data,
+      opt.format,
+    );
+  } catch (error) {
+    console.error(
+      'There was an issue in WRITING the data to the location: ',
+      opt.exportFileLocation,
+    );
+
+    if (opt.devMode === true) console.error(error);
+
+    return; // stopping the execution of the script, with some grace, hopefully.
+  }
 }
 
 module.exports = write;
--- scipts/get-data.js
+++ scipts/get-data.js
@@ -2,7 +2,18 @@ const fs = require('fs').promises;
 const opt = require('../config.js');
 
 async function read() {
-  await fs.readFile(opt.importFileLocation, opt.format);
+  try {
+    return await fs.readFile(opt.importFileLocation, opt.format);
+  } catch (error) {
+    console.error(
+      'There was an issue IMPORTING the data from the location:',
+      opt.importFileLocation,
+    );
+
+    if (opt.devMode === true) console.error(error);
+
+    return; // stopping the execution of the script, with some grace, hopefully.
+  }
 }
 
 module.exports = read;
