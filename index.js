7c24d7ede733e93aec9d2e30f401560284617f67
Eric <vespers-garages.0n@icloud.com>
2025-05-28 09:56:03 -0400

    listing previous apps in table on terminal.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const questions = require('./scipts/questions.js');
 const read = require('./scipts/get-data.js');
 const write = require('./scipts/export-data.js');
+const csvToObj = require('./scipts/csv-to-obj.js');
+const filterObj = require('./scipts/filter-object.js');
 const opt = require('./config.js');
 
 async function main() {
@@ -12,37 +14,62 @@ async function main() {
 
   console.log(WELCOME_MESSAGE);
 
-  try {
-    const DATA = await read(opt.importFileLocation, opt.format);
+  if (opt.devMode === true) {
+    console.log(
+      '\n*** DEV MODE is enabled *** File output and backup are DISABLED in this mode. \n',
+    );
 
-    if (!DATA) {
-      throw 'DATA read was undefined or otherwise not usable.';
-    }
-
-    const WRITE_BACKUP_FILE = opt.backup.writeBackupFile;
-
-    if (WRITE_BACKUP_FILE === true) {
-      //
-      write(opt.backup.backupFileLocation, DATA, opt.format);
-    }
-
-    const NEW_JOBS = await questions();
-
-    if (!NEW_JOBS) {
-      throw 'NEW APPLICATION was undefined or otherwise not usable.';
-    }
-
-    write(
-      opt.exportFileLocation,
-      DATA + opt.splitOn + NEW_JOBS,
+    const DATA_PREV = await read(
+      opt.importFileLocation,
       opt.format,
     );
-  } catch (error) {
-    console.log();
 
-    if (opt.devMode === true) console.error(error);
-  } finally {
-    console.log('Script Done.');
+    const DATA_OBJ = csvToObj(DATA_PREV);
+
+    const FILTERED = filterObj(DATA_OBJ.data);
+
+    console.table(FILTERED, [
+      'Company',
+      'Role',
+      'DateApplied',
+      'Id',
+    ]);
+  } else {
+    // DEV MODE OFF
+    try {
+      const DATA_PREV = await read(
+        opt.importFileLocation,
+        opt.format,
+      );
+
+      if (!DATA_PREV) {
+        throw 'DATA_PREV read was undefined or otherwise not usable.';
+      }
+
+      if (opt.backup.writeBackupFile === true) {
+        write(
+          opt.backup.backupFileLocation,
+          DATA_PREV,
+          opt.format,
+        );
+      }
+
+      const DATA_NEW = await questions();
+
+      if (!DATA_NEW) {
+        throw 'NEW APPLICATION was undefined or otherwise not usable.';
+      }
+
+      write(
+        opt.exportFileLocation,
+        DATA_PREV + opt.splitOn + DATA_NEW,
+        opt.format,
+      );
+    } catch (error) {
+      console.error(error);
+    } finally {
+      console.log('Script Complete.');
+    }
   }
 }
 
