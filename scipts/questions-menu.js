4964bd1c5f400e5487c73f5f701fb31517caeb1d
Eric <vespers-garages.0n@icloud.com>
2025-05-30 10:35:28 -0400

    menu control

---
+++ scipts/questions-menu.js
@@ -0,0 +1,25 @@
+const { select } = require('@inquirer/prompts');
+
+async function MenuQuestions() {
+  //
+  return await select({
+    message: 'Select Menu Option',
+    choices: [
+      {
+        name: 'Add Application',
+        value: 'add',
+      },
+      {
+        name: 'List Previous Applications',
+        value: 'list',
+      },
+      {
+        name: 'Update Previous Applications',
+        value: 'update',
+      },
+    ],
+  });
+}
+
+module.exports = MenuQuestions;
+
