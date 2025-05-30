36266be59d403e23a23a2c0f428750adc3043a16
Eric <vespers-garages.0n@icloud.com>
2025-05-30 14:16:34 -0400

    added truncation for those records that were getting too lengthy

--- scipts/filter-object.js
+++ scipts/filter-object.js
@@ -3,9 +3,16 @@
 function FilterObject(toFilter) {
   return Object.keys(toFilter).map((key) => {
     return {
-      Company: toFilter[key].Company,
+      Company:
+        toFilter[key].Company.length > 25
+          ? toFilter[key].Company.slice(0, 25) + '...'
+          : toFilter[key].Company,
       Role: toFilter[key].Role,
       DateApplied: toFilter[key].DateApplied || '',
+      Salary:
+        toFilter[key].Salary.length > 20
+          ? toFilter[key].Salary.slice(0, 25) + '...'
+          : toFilter[key].Salary || '$000',
       Id: toFilter[key].Id || '',
     };
   });
