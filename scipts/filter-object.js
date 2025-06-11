d225b9d630bc9c5387159b287780cb94024d059e
Eric <vespers-garages.0n@icloud.com>
2025-06-11 15:04:30 -0400

    removed salary from filter

--- scipts/filter-object.js
+++ scipts/filter-object.js
@@ -2,23 +2,28 @@
 const opt = require('../config');
 
 function FilterObject(toFilter) {
-  return Object.keys(toFilter).map((key) => {
-    return {
-      Company:
-        toFilter[key].Company.length > opt.columnWidth
-          ? toFilter[key].Company.slice(0, opt.columnWidth) +
-            '...'
-          : toFilter[key].Company,
-      Role: toFilter[key].Role,
-      DateApplied: toFilter[key].DateApplied || '',
-      Salary:
-        toFilter[key].Salary.length > opt.columnWidth
-          ? toFilter[key].Salary.slice(0, opt.columnWidth) +
-            '...'
-          : toFilter[key].Salary || '$000',
-      Id: toFilter[key].Id || '',
-    };
-  });
+  try {
+    if (!toFilter) {
+      throw 'There is no incoming data to filter...';
+    }
+
+    const FILTERED = Object.keys(toFilter).map((key) => {
+      return {
+        Company:
+          toFilter[key].Company.length > opt.columnWidth
+            ? toFilter[key].Company.slice(0, opt.columnWidth) +
+              '...'
+            : toFilter[key].Company,
+        Role: toFilter[key].Role,
+        DateApplied: toFilter[key].DateApplied || '',
+        Id: toFilter[key].Id || '',
+      };
+    });
+
+    return FILTERED;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = FilterObject;
