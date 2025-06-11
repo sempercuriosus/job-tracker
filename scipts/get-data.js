6bad38325696b20fe0e7dd3723dcd3a7f2e4399b
Eric <vespers-garages.0n@icloud.com>
2025-06-11 12:28:07 -0400

    added csv to object conversion here

--- scipts/get-data.js
+++ scipts/get-data.js
@@ -1,19 +1,24 @@
 const fs = require('fs').promises;
+const csv_obj = require('./csv-object.js');
 
 async function read(importLocation, format) {
   try {
     const DATA = await fs.readFile(importLocation, format);
 
     if (!DATA) {
-      throw 'DATA_PREV read was undefined or otherwise not usable.';
+      throw 'DATA read was undefined or otherwise not usable.';
     }
 
-    return DATA;
+    const DATA_AS_OBJECT = await csv_obj.CsvToObj(DATA);
+
+    return DATA_AS_OBJECT;
   } catch (error) {
     console.error(
       'There was an issue IMPORTING the data from the location:',
       importLocation,
     );
+
+    console.error(error);
 
     return; // stopping the execution of the script, with some grace, hopefully.
   }
